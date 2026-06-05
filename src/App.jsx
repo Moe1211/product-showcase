4d9588d6f6a27bc404a28071e2a0d0e7ba8af86f
N1nja <ibrahim.moe121@gmail.com>
2026-06-05 12:01:32 +0300

    Implement Highlights Section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Showcase from './components/Showcase';
 import Performance from './components/Performance';
 import Features from './components/Features'
 import Footer from './components/Footer'
+import Highlights from './components/Highlights'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -20,6 +21,7 @@ const App = () => {
       <Showcase />
       <Performance />
       <Features />
+      <Highlights />
       <Footer />
     </main>
   );
